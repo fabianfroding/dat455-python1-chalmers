1d5f4b5e82d6f738062a77919506fa5362d54d5c
fabianfroding <fabian.froding@gmail.com>
2020-07-04 11:49:45 +0200

    progress matmul impl

--- lab3/matrix.py
+++ lab3/matrix.py
@@ -29,22 +29,28 @@ def powers(list, num1, num2):
 
 
 def matmul(mat1, mat2):
-    mat1Rows = len(mat1[0])
-    mat1Cols = len(mat1)
-    mat2Rows = len(mat2[0])
-    mat2Cols = len(mat2)
-    newMat = [[0 for x in range(mat1Rows)] for y in range(mat1Cols)]
+    if mat1 != [] and mat2 != []:
 
-    for i in range(0, mat1Cols):
-        for j in range(0, mat1Rows):
-            k = mat1[i][j] * mat2[j][i]
-            #print(mat1[i][j])
-            print(mat2[mat2Rows - 1 - j][mat2Cols - 1 - i])
-            #print(k)
-            #print(str(mat1[i][j]) + " * " + str(mat2[j][i]) + " = " + str(k))
-            #newMat[i][j] = mat1[i][j]
+        # Setup dimensions of result matrix
+        newMatRows = len(mat1) if len(mat1) > len(mat2) else len(mat2)
+        newMatCols = len(mat1[0]) if len(mat1[0]) > len(mat2[0]) else len(mat2[0])
+        newMat = [[0 for x in range(newMatRows)] for y in range(newMatCols)]
 
-    return newMat
+        print(newMatRows)
+        print(newMatCols)
+
+        # Row of mat 1
+        for i in range(len(mat1)):
+            # Col of mat 2
+            for j in range(len(mat2[0])):
+                res = 0
+                # Row of mat 2
+                for k in range(len(mat2)):
+                    res = mat1[i][k] * mat2[k][j]
+                    newMat[i][j] += res
+
+        return newMat
+    return []
 
 
 # def invert():
@@ -59,6 +65,7 @@ def matmul(mat1, mat2):
 
 # print(transpose([[1, 2], [3, 4], [5, 6]]))
 
+
 # print(powers([2, 3, 4], 0, 2)) # [[1, 2, 4], [1, 3, 9], [1, 4, 16]]
 # print(powers([], 0, 10)) # []
 # print(powers([2], 0, 2)) # [[1, 2, 4]]
@@ -66,7 +73,17 @@ def matmul(mat1, mat2):
 # print(powers([2], 0, -1)) # [[]])
 # print(powers([2, 3], 0, 2)) # [[1, 2, 4], [1, 3, 9]])
 
-s1 = [[0, 1], [1, 0]]
-s3 = [[1, 0], [0, -1]]
 
-print(matmul(s1, s3))
+#print(matmul([[0, 1], [1, 0]], [[1, 0], [0, -1]]))
+#print(matmul([], [])) # []
+#print(matmul([[2]], [[4]])) # [[8]]
+#print(matmul([[2, 1]], [[4], [3]])) # [[11]]
+#print(matmul([[1, 2], [3, 4]], [[0, 1], [1, 0]])) # [[2, 1], [4, 3]]
+#print(matmul([[1, 2], [3, 4]], [[1, 0], [0, 1]])) # [[1, 2], [3, 4]]
+#print(matmul([[1, 2], [3, 4], [5, 6]], [[1, 1, 1], [1, 1, 1]])) # [[3, 3, 3], [7, 7, 7], [11, 11, 11]]
+print(matmul([[1, 2, 3], [4, 5, 6]], [[7, 8, 9, 10], [11, 12, 13, 14], [15, 16, 17, 18]]))
+# [[74, 80, 86, 92], [173, 188, 203, 218]]
+#print(matmul([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[1, 0, 0], [0, 1, 0], [0, 0, 1]]))
+# [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+#print(matmul([[1, 0, 0], [0, 1, 0], [0, 0, 1]], [[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+# [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
